6aedf43682d8162f0540752807c276026d458bdf
spw32767 <suphawit.c@kkumail.com>
2025-12-01 16:13:50 +0700

    Squashed 'fund-management-api/' changes from 5237bf0..62678ab
    62678ab preserve table metadata in GORM session for accurate count in ListAll method
    d3165f9 add admin endpoint to list Scopus publications with pagination and search
    
    git-subtree-dir: fund-management-api
    git-subtree-split: 62678abd9dd9b742ecd7ae08dab987bb120a0599

--- routes/routes.go
+++ routes/routes.go
@@ -399,6 +399,7 @@ func SetupRoutes(router *gin.Engine) {
 				admin.GET("/user-publications/import/scholar/runs", controllers.AdminListScholarImportRuns)
 				admin.POST("/user-publications/import/scopus", controllers.AdminImportScopusPublications)
 				admin.POST("/user-publications/import/scopus/all", controllers.AdminImportScopusForAll)
+				admin.GET("/publications/scopus", controllers.AdminListScopusPublications)
 				admin.GET("/scopus/config", controllers.AdminGetScopusAPIKey)
 				admin.PUT("/scopus/config", controllers.AdminUpdateScopusAPIKey)
 				admin.GET("/user-publications/scholar/search", controllers.TeacherScholarAuthorSearch)
